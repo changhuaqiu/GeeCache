adfefc074a4fe4505a28a3c9a8f909c18c3dcf18
ubuntu <ubuntu@localhost.localdomain>
2021-12-05 23:12:05 +0800

    add

--- lru/lru.go
+++ lru/lru.go
@@ -37,3 +37,32 @@ func (c *Cache) Get(key string) (value Value, ok bool) {
 	}
 	return
 }
+
+func(c *Cache)RemoveOldest(){
+	ele := c.ll.back();
+	if ele != nil {
+		c.ll.remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache,kv.key)
+		c.nbytes -= int64(len(kv.key)) +  int64(kv.Value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key,kv.Value)
+		}
+	}
+}
+//新增 修改
+func(c *Cache) Add (key string,value Value){
+	if ele ,ok := c.cache[key]; ok{
+		c.ll.MoveToFront(ele)
+		kv := ele.Vlaue.(*entry)
+		c.nbytes  += int64(value.Len()) - int(kv.value.Len())
+		kv.value = value
+	}else{
+		ele := c.ll.PushFront(&entry{key,value})
+		c.cache[key] = value
+		c.nbytes += int64(value.Len()) + int64(len(kv.key))
+	}
+	for c.nbytes != 0 && c.maxBytes < c.nbytes{
+		c.RemoveOldest()
+	}
+}
